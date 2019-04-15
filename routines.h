f6ed3e7e53814bb0e6f8d1918b5d69b911229f03
Benker <lyleungad@ust.hk>
2019-04-15 22:20:31 +0800

    modified way of routine

---
+++ routines.h
@@ -0,0 +1,50 @@
+/**
+ *  This function helps to handle the https request
+ * 
+ *  Return?
+ * 
+ */
+int https_routine(char* req_header) {
+
+}
+
+/**
+ *  This function helps to handle the http with cache miss in local
+ * 
+ *  Return?
+ * 
+ */
+int no_cache_routine(char* req_header) {
+
+}
+
+
+/**
+ *  This function helps to handle the http with cache hit in local
+ * 
+ *  Return?
+ * 
+ */
+int cache_routine(char* req_header) {
+
+}
+/**
+ *  This function helps to handle the cache hit but outdate
+ * 
+ *  Return?
+ */
+int outdate_cache_routine(char* req_header) {
+
+}
+/**
+ *  This function helps to handle the cache hit and not outdate
+ * 
+ *  Return?
+ */
+int indate_cache_routine(char* req_header) {
+
+}
+
+
+
+
